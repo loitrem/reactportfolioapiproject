5d40da940234c643bc2a3e2ee7ede8cda950b76d
loitrem <ski_fix@hotmail.com>
2023-07-31 16:33:47 -0400

    minor change

--- src/models/apikeys.js
+++
@@ -1,6 +0,0 @@
-const keys = {
-    REACT_APP_GITHUB_API_KEY: "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",
-    REACT_APP_WEATHER_API_KEY: "8b923b04d5a9435a9ab155803233107"
-}
-
-module.exports = keys
